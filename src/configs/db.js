44a08b3d34810cd0a337aa4fb135558cdd14cd9b
FIDEL GOLDSMAN <49002222@ort>
2026-05-18 09:07:48 -0300

    working

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,25 @@
-import postgres from 'postgres';
+import pg from 'pg';
 import 'dotenv/config';
 
-const sql = postgres(process.env.DATABASE_URL);
+const { Pool } = pg;
 
-export default sql;
+const config = {
+  host     : process.env.DB_HOST,
+  database : process.env.DB_NAME,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  port     : process.env.DB_PORT,
+  ssl      : { rejectUnauthorized: false } // requerido por Supabase
+};
+
+const pool = new Pool(config);
+
+pool.connect()
+  .then(() => {
+    console.log("✅ Conectado a PostgreSQL");
+  })
+  .catch((err) => {
+    console.error("❌ Error de conexión:", err.message);
+  });
+
+export default pool;
